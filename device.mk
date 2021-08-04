688f246192691bcf32eba84f539d1261cd25d5ea
Lucifer-morning-star-96 <MSnell3130@student.egcc.edu>
2021-08-04 16:18:08 +0530

    lavender : Use Xiaomi Charger Animation dropped pixel offline charging animation
    Signed-off-by: Lucifer-morning-star-96 <Msnell3031@gmail.com>

--- device.mk
+++ device.mk
@@ -237,9 +237,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-service
-
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # IFAA manager
 PRODUCT_PACKAGES += \
